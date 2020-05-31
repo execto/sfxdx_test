97395d62a657ac8e9c72ee6494800f02459403ea
elahov <sasha-elahov@yandex.ru>
2020-05-31 12:00:44 +0300

    init workspace

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import './index.scss';
+
+const node = document.getElementById('app');
+ReactDOM.render(<div className="app">Hello</div>, node);
---
+++ src/store.ts
@@ -0,0 +1,5 @@
+import { createStore } from 'redux';
+
+const initialState = [];
+
+export const strore = createStore();
